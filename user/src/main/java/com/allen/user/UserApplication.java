43c49d7ebbb972916caabc714e8d4fa2c22bd3a1
allen <allenwangook@gmail.com>
2019-10-20 10:05:41 +0800

    kafka different group test

--- user/src/main/java/com/allen/user/UserApplication.java
+++ user/src/main/java/com/allen/user/UserApplication.java
@@ -1,6 +1,7 @@
 package com.allen.user;
 
 import com.allen.user.kafka.KafKaSender;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -11,10 +12,11 @@ public class UserApplication {
     public static void main(String[] args) {
 
         ConfigurableApplicationContext context=SpringApplication.run(UserApplication.class, args);
-//        KafKaSender kafKaSender= context.getBean(KafKaSender.class);
-//        for (int i = 0; i <50 ; i++) {
-//            kafKaSender.senMessage("allen_topic_test","sssssss"+i);
-//        }
+
+        KafKaSender kafKaSender= context.getBean(KafKaSender.class);
+        for (int i = 0; i <100 ; i++) {
+            kafKaSender.senMessage("allen_topic_test","sssssss"+i);
+        }
     }
 
 }
--- user/src/main/java/com/allen/user/kafka/KafKaConsumer.java
+++ user/src/main/java/com/allen/user/kafka/KafKaConsumer.java
@@ -1,9 +1,11 @@
 package com.allen.user.kafka;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -12,15 +14,17 @@ import java.util.Optional;
  */
 @Component
 public class KafKaConsumer {
-    @KafkaListener(topics = "allen_topic_test",groupId = "allen_kafka2")
-    public void listen(ConsumerRecord<?,?> record){
-        Optional<?> kafkaMessage=Optional.ofNullable(record.value());
-        if (kafkaMessage.isPresent()){
-            Object message=kafkaMessage.get();
-            System.out.println("*****************");
-            System.out.println(message);
-            System.out.println("*****************");
+    @KafkaListener(topics = "allen_topic_test",groupId = "allen_kafka")
+    public void listen(ConsumerRecords<String, String>records){
+        for (ConsumerRecord<String, String> record:records){
+            Optional<?> kafkaMessage=Optional.ofNullable(record.value());
+            if (kafkaMessage.isPresent()){
+                Object message=kafkaMessage.get();
+                System.out.println("*****************");
+                System.out.println(message);
+            }
         }
+        System.out.println("*****************本次消费数量:"+records.count());
 
     }
 }
